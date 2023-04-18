2463909157a7409120bc415bf3fbe06b7093daaf
JonahPeck <PeckJonahC@gmail.com>
2023-04-18 12:34:37 -0600

    models

--- server/models.py
+++ server/models.py
@@ -5,85 +5,97 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-})
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+      })
 db = SQLAlchemy(metadata=metadata)
-
 
 class Trail(db.Model, SerializerMixin):
     __tablename__ = 'trails'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
-    url = db.Column(db.String)
-    length = db.Column(db.Float)
+    url	= db.Column(db.String)
+    length	= db.Column(db.Float)
     description = db.Column(db.String)
     # directions	= db.Column(db.String)
-    city = db.Column(db.String)
-    region = db.Column(db.String)
-    country = db.Column(db.String)
-    lat = db.Column(db.Float)
-    lon = db.Column(db.Float)
-    difficulty = db.Column(db.String)
+    city	= db.Column(db.String)
+    region	= db.Column(db.String)
+    country	= db.Column(db.String)
+    lat	= db.Column(db.Float)
+    lon	= db.Column(db.Float)
+    difficulty	= db.Column(db.String)
     features = db.Column(db.String)
-    rating = db.Column(db.Float)
+    rating	= db.Column(db.Float)
     thumbnail = db.Column(db.String)
 
     # latitude = db.Column(db.Integer)
     # longitude = db.Column(db.Integer)
     votes = db.Column(db.Integer)
 
+    comments = db.relationship("Comment", backref = "trail")
+    usertrails = db.relationship( 'UserTrail', backref = 'trail' )
+
+
+    serialize_rules = ('-comments','-comments.trails' ,'-created_at', '-updated_at')
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(
-        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
 
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
     email = db.Column(db.String)
     password = db.Column(db.String)
-
+    profile_picture = db.Column(db.String)
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(
-        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
 
     # relationships have jonah check cus what is going on here
-    usertrails = db.relationship('Usertrails', backref='users')
-    trails = association_proxy('user_trails', 'trails')
+    usertrails = db.relationship( 'UserTrail', backref = 'user' )
+    trails = association_proxy( 'user_trails', 'trails' )
 
-    serialize_rules = ('-user_trails', '-trails.users',
-                       '-created_at', '-updated_at')
+    comments = db.relationship( 'Comment', backref = 'user' )
+
+    serialize_rules = ('-usertrails','-trails.users' ,'-created_at', '-updated_at')
 
 
 class Comment(db.Model, SerializerMixin):
     __tablename__ = 'comments'
 
-    id = db.Column(db.Integer, primary_key=True)
-    trail_id = db.Column(db.Integer, db.ForeignKey('trails.id'))
+
+    id = db.Column(db.Integer, primary_key = True)
+    trail_id =db.Column(db.Integer, db.ForeignKey('trails.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     content = db.Column(db.String)
     votes = db.Column(db.Integer)
 
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(
-        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
+    serialize_rules = ('-user.comments','trail.comments','-trails.users' ,'-created_at', '-updated_at')
 
 class UserTrail(db.Model, SerializerMixin):
     __tablename__ = 'user_trails'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    trail_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    trail_id = db.Column(db.Integer, db.ForeignKey('trails.id'))
     ridden = db.Column(db.Boolean)
     wishlist = db.Column(db.Boolean)
 
+    serialize_rules = ('-user.usertrails','trail.usertrails','-trails.users' ,'-created_at', '-updated_at')
+
+    
+    
     # serialize_rules = ('-leases','-apartments.tenants' ,'-created_at', '-updated_at')
 
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(
-        db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
