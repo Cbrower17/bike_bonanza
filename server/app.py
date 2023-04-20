a50ba0925ccdaf141c6f86db321626e476579611
JonahPeck <PeckJonahC@gmail.com>
2023-04-20 09:57:22 -0600

    merge

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@ from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from flask_cors import CORS
-from flask_bcrypt import Bcrypt
-from services import app,db,bcrypt
+from services import app,db
 
 from models import db, User, UserTrail, Trail, Comment
 
@@ -31,6 +30,19 @@ class AllTrails(Resource):
             dict_trails.append(trail.to_dict())
         return make_response(dict_trails, 200)
 
+    def post(self):
+        try:
+            input = request.get_json()
+            new_comment = User(
+                name=input['name'], email=input['email'], password=input['password'])
+            db.session.add(new_comment)
+            db.session.commit()
+            return make_response(new_comment.users.to_dict(), 201)
+        except:
+            return make_response({
+                "errors": ["validation errors"]
+            }, 400)
+
 
 api.add_resource(AllTrails, '/trails')
 
@@ -56,7 +68,6 @@ class Users(Resource):
         for user in all_users:
             dict_users.append(user.to_dict())
         return make_response(dict_users, 200)
-    
 
 
 api.add_resource(Users, '/users')
@@ -79,15 +90,13 @@ class AddUser(Resource):
         try:
             input = request.get_json()
             new_user = User(
-                name=input['name'], email=input['email'])
-            new_user.password_hash = input['password']
-            print(new_user.to_dict())
+                name=input['name'], email=input['email'], password=input['password'])
             db.session.add(new_user)
             db.session.commit()
-            return make_response(new_user.to_dict(), 201)
-        except Exception as e:
+            return make_response(new_user.users.to_dict(), 201)
+        except:
             return make_response({
-                "errors": [str(e)]
+                "errors": ["validation errors"]
             }, 400)
 
 
@@ -142,16 +151,6 @@ class check_login(Resource):
             res = make_response(jsonify(user.to_dict()),200)
             return res
 api.add_resource(check_login, '/checklogin')
-
-class check_logged_in(Resource):
-    def get(self):
-        user_id = session.get('user_id')
-        if user_id:
-            if user_id != None:
-                return make_response({"logged_in": True},200)
-        return make_response({"logged_in": False},200)
-    
-api.add_resource(check_logged_in, '/check')
 
 class logout(Resource):
     def delete(self):
